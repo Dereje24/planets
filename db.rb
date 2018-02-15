9b87e7fa7ebb3de258e65084f4bf73d8ac069581
Dev <dev@divethree.com>
2018-02-15 08:19:34 -0800

    Update instructions.

--- db.rb
+++ db.rb
@@ -4,7 +4,7 @@
 # We're establishing our first connection to a database in this first exercise. To do so, we first need to create a
 # new database.
 #     - Open a terminal window
-#     - Type the following command: `createdb learndb`
+#     - Type the following command: `createdb library`
 #     - This will create a database with your currently logged in system user; the default user is postgres. Remember.
 
 # Import our Postgres module so that we can connect to the database.
@@ -22,15 +22,19 @@ end
 connection.exec('CREATE TABLE books (id SERIAL PRIMARY KEY,
                                      title VARCHAR(255))')
 
+p "Table created: books."
+
 # TODO: Change this to a method that inserts a new author into the authors table
 connection.exec("INSERT INTO books (title) VALUES ('It')")
+
+p "Book added: It."
 
 # Output a table of current data in our database by running this script: `ruby db.rb`
 # You'll see it print out a table of the current contents of our database
 connection.exec( "SELECT * FROM books" ) do |result|
-  puts "ID | Email"
+  puts "ID | title"
   result.each do |row|
-    puts " %s | %s" % row.values_at('id', 'email')
+    puts " %s | %s" % row.values_at('id', 'title')
   end
 end
 
